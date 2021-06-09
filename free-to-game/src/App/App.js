a9f78d7a4f3d761758de1e6b6f0600d9d56c2538
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-09 18:54:02 -0400

    Add fetch call to app

--- free-to-game/src/App/App.js
+++ free-to-game/src/App/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect }from 'react'
+import fetchGameData from '../ApiData/ApiCall'
+import cleanGameData from '../ApiData/CleanApicall'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [ freeGames, setFreeGames] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    callGames()
+  },[freeGames])
+
+  callGames = async () => {
+    try{
+      const gameData = fetchGameData()
+      setFreeGames(cleanGameData(gameData))
+    }catch{
+
+    }
+  }
   return (
-    <div className="App">
+    <div>
+      
     </div>
-  );
+  )
 }
-
-export default App;
