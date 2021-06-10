506137b127ea4833c7411532138ed487ed344618
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-10 18:21:20 -0400

    Move files to root

--- src/ApiData/ApiCall.js
+++ src/ApiData/ApiCall.js
@@ -1,12 +1,10 @@
-const fetchGameData = async () => {
+export default async function fetchGameData() {
   const response = await fetch("https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": process.env.REACT_APP_API_KEY,
-		"x-rapidapi-host": process.env.REACT_APP_API_HOST
+		"x-rapidapi-key": process.env._API_KEY,
+		"x-rapidapi-host": process.env._API_HOST
 	}
 })
   return response.json()
 }
-
-export default fetchGameData
