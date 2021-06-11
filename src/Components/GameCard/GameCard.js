d675832c06b40bdaf0b2a667bbf00b34f45f0001
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-11 11:57:59 -0400

    Add link to all cards

--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function GameCard({ thumbnail, description, title, genre }) {
   return (
-    <div>
+    <Link to=':id'>
       <img src={thumbnail} alt={`Poster for ${title}`}></img>
       <h3>{title}</h3>
       <p>{description}</p>
       <p>{genre}</p>
-    </div>
+    </Link>
   )
 }
