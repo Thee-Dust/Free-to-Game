f7e148602e5902868e1e67fc8ecdb2e9919da40c
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-11 18:33:54 -0400

    Add information for game details page

--- src/Components/GameDetails/GameDetails.js
+++ src/Components/GameDetails/GameDetails.js
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import fetchGameData from '../../ApiData/ApiCall'
 import { Link } from 'react-router-dom'
 
-export default function GameDetails({ game }) {
-  
-  return (
-    <div>
-      <Link to ='/'><i className="fas fa-arrow-left"></i></Link>
-      <img src={thumbnail} alt={`poster for ${title}`} />
-      <a href={url}>Download now</a>
-      <button>Add to Wishlist</button>
-      <span>{title}</span>
-      <p>{description}</p>
-      <p>{genre}</p>
-      <p>{developer}</p>
-      <p>{releaseDate}</p>
-    </div>
-  )
+export default function GameDetails({ id }) {
+  const [ freeGame, setFreeGame ] = useState('')
+  const [ error, setError ] = useState('')
+
+  useEffect(() => {
+    const callGames = async () => {
+      setError('')
+      try{
+        const gameData = await fetchGameData(`game?id=${id}`)
+        setFreeGame(gameData)
+      }catch{
+        setError('Failed to recieve games')
+      }
+    }
+    callGames()
+  },[id])
+
+
+  if(freeGame){
+    return (
+      <div>
+        <Link to ='/'><i className="fas fa-arrow-left"></i></Link>
+        <img src={freeGame.thumbnail} alt={`poster for ${freeGame.title}`}/>
+        <div>
+          <h2>{freeGame.title}</h2>
+          <div>
+          <button>Add to wishlist</button>
+          <a href={freeGame.game_url}>Download now</a>
+          </div>
+        </div>
+        <div>
+          <p>{freeGame.description}</p>
+          <div>
+            <span>ABOUT</span>
+            <p>{freeGame.genre}</p>
+            <p>{`Developed by ${freeGame.developer}`}</p>
+            <span>System requirements:</span>
+            <ul>
+              <li>{`Graphics: ${freeGame.minimum_system_requirements.graphics}`}</li>
+              <li>{`Storage: ${freeGame.minimum_system_requirements.storage}`}</li>
+              <li>{`Memory: ${freeGame.minimum_system_requirements.memory}`}</li>
+              <li>{`Processor: ${freeGame.minimum_system_requirements.processor}`}</li>
+              <li>{`Operating System: ${freeGame.minimum_system_requirements.os}`}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return null
 }
