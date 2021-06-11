58458a2b5bfe16d027c33f50a3d2d47cff6c49cd
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-10 22:01:06 -0400

    Add key to all cards

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,6 +6,7 @@ export default function Home({ games }) {
     const {id, title, thumbnail, description, url, genre, publisher, releaseDate} = game
     return (
       <GameCard
+      key={id}
       id={id}
       title={title}
       thumbnail={thumbnail}
