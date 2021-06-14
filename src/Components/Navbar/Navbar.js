ad10c9e143bd4f74f2f7c2ef2de87fb4bb04a629
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-14 13:17:20 -0400

    Add style to navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,18 +4,22 @@ import './Navbar.css'
 
 export default function Navbar({ findSearch }) {
   const searchRef = useRef()
+  const path = window.location.pathname
+  console.log(path)
   return (
     <header>
       <Link to='/'><h1>Free 2 Game</h1></Link>
-      <input type='text' ref={searchRef} onChange={(() => findSearch(searchRef.current.value))} placeholder='Search games here'></input>
-      <ul className="user-display">
-        <li><span>UserName</span>
-          <ul className='user-options'>
-            <li><Link to='/wishlist'>Wishlist</Link></li>
-            <li><span>Log out</span></li>
-          </ul>
-        </li>
-      </ul>
+      <div className='head-options'>
+        <input type='text' ref={searchRef} onChange={(() => findSearch(searchRef.current.value))} placeholder='Search games here'></input>
+        <ul className="user-display">
+          <li><span>UserName</span>
+            <ul className='user-options'>
+              <li><Link to='/wishlist'>Wishlist</Link></li>
+              <li><span>Log out</span></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
