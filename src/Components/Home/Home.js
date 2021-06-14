25c891fbb5a6e03ab550cda3aa6a8b9cd2d23ccc
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-14 12:12:49 -0400

    Add if statement for search cards

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,41 +1,9 @@
-import React, {useRef} from 'react'
+import React from 'react'
 import GameCard from '../GameCard/GameCard'
 import PropTypes from 'prop-types'
 import './Home.css'
 
-export default function Home({ games, error, searchedGames, findSearch }) {
-  const searchRef = useRef()
-
-  if(searchedGames.length) {
-    const searchResults = games.map(game.title.toLowerCase().includes(searchedGames.toLowerCase()))
-    const searchCards = searchResults.map(game => {
-    const {id, title, thumbnail, description, genre } = game
-    return (
-      <GameCard
-      key={id}
-      id={id}
-      title={title}
-      thumbnail={thumbnail}
-      description={description}
-      genre={genre}
-      />
-    )
-  })
-    return (
-      <div className='home'>
-        {error && <h1>{error}</h1>}
-        {!games && !error &&
-        <div className='loading-box'>
-          <h1>Loading....</h1>
-          <h2>Going to Gamestop to fetch the games</h2>
-        </div>}
-        <div className='card-section'>
-          <input type='text' ref={searchRef} onChange={(() => findSearch(searchRef))} placeholder='Search games here'></input>
-          {gameCards}
-        </div>
-      </div>
-    )
-  }
+export default function Home({ games, error, searchedGames }) {
 
   const gameCards = games.map(game => {
     const {id, title, thumbnail, description, genre } = game
@@ -50,6 +18,19 @@ export default function Home({ games, error, searchedGames, findSearch }) {
       />
     )
   })
+
+  if(searchedGames){
+    const searchResults = gameCards.map(game => game.props.title.toLowerCase().includes(searchedGames.toLowerCase()))
+    return(
+      <div className='home'>
+        {!searchResults.length && <h1>It looks like no movies match your search. Please try searching something else</h1>}
+        <div className='card-section'>
+        {searchResults}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='home'>
       {error && <h1>{error}</h1>}
@@ -59,7 +40,6 @@ export default function Home({ games, error, searchedGames, findSearch }) {
         <h2>Going to Gamestop to fetch the games</h2>
       </div>}
       <div className='card-section'>
-        <input type='text' ref={searchRef} onChange={(() => findSearch(searchRef))} placeholder='Search games here'></input>
         {gameCards}
       </div>
     </div>
@@ -68,5 +48,6 @@ export default function Home({ games, error, searchedGames, findSearch }) {
 
 Home.prototype = {
   games: PropTypes.array,
-  error: PropTypes.string
+  error: PropTypes.string,
+  searchedGames: PropTypes.string
 };
