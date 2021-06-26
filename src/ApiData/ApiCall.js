754300c94c02ff5ccb3d986e46c0764b67dd30cd
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-25 21:50:25 -0400

    Fix bug

--- src/ApiData/ApiCall.js
+++ src/ApiData/ApiCall.js
@@ -2,7 +2,7 @@ export default async function fetchGameData(query) {
   const response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/${query}`, {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": '67f1bd6b87mshb743c6ea9a6cbacp1d3c85jsn3fc5852b98ce',
+		"x-rapidapi-key": process.env.FREE_TO_GAME_API_KEY,
 		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com"
 	}
 })
